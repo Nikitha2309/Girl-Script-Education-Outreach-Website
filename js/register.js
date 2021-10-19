dfdc6cdba4522c53c1e45bff4f57260d677e88a4
everly <everlyprecia@gmail.com>
2021-10-19 14:12:54 +0530

    fix responsiveness issue

--- js/register.js
+++ js/register.js
@@ -2,21 +2,26 @@ function next(){
         document.getElementById('student-form2').style.left="15px";
         document.getElementById('student-form2').style.right="15px";
         document.getElementById('student-form1').style.left="1000px";
-        
+        document.getElementById("student-form2").style.opacity = "1";
+        document.getElementById("student-form1").style.opacity = "0";
     };
 
 function back(){
     document.getElementById('student-form1').style.left="15px";
     document.getElementById('student-form1').style.right="15px";
     document.getElementById('student-form2').style.left="1000px";
-    
+    document.getElementById('student-form2').style.opacity="0";
+    document.getElementById("student-form1").style.opacity = "1";
 }
 
 function mentorform(){
     document.getElementById('student-form1').style.left="2000px";
+    document.getElementById('student-form1').style.opacity="0";
     document.getElementById('student-form2').style.left="2000px";
+    document.getElementById('student-form2').style.opacity="0";
     document.getElementById('mentor-form1').style.left="15px";
     document.getElementById('mentor-form1').style.right="15px";
+    document.getElementById('mentor-form1').style.opacity="1";
     document.getElementById('mentor').style.backgroundColor="#EB5310";
     document.getElementById('mentor').style.color="#ffffff";
     document.getElementById('student').style.backgroundColor="#ffffff";
@@ -24,9 +29,12 @@ function mentorform(){
 }
 function studentform(){
     document.getElementById('mentor-form1').style.left="2000px";
+    document.getElementById('mentor-form1').style.opacity="0";
     document.getElementById('mentor-form2').style.left="2000px";
+    document.getElementById('mentor-form2').style.opacity="0";
     document.getElementById('student-form1').style.left="15px";
     document.getElementById('student-form1').style.right="15px";
+    document.getElementById('student-form1').style.opacity="1";
     document.getElementById('student').style.backgroundColor="#EB5310";
     document.getElementById('student').style.color="#ffffff";
     document.getElementById('mentor').style.backgroundColor="#ffffff";
@@ -35,10 +43,14 @@ function studentform(){
 function mentornext() {
     document.getElementById('mentor-form2').style.left="15px";
     document.getElementById('mentor-form2').style.right="15px";
+    document.getElementById('mentor-form2').style.opacity="1";
     document.getElementById('mentor-form1').style.left="1000px";
+    document.getElementById('mentor-form1').style.opacity="0";
 }
 function mentorback(){
     document.getElementById('mentor-form1').style.left="15px";
     document.getElementById('mentor-form1').style.right="15px";
+    document.getElementById('mentor-form1').style.opacity="1";
     document.getElementById('mentor-form2').style.left="1000px";
+    document.getElementById('mentor-form2').style.opacity="0";
 }
