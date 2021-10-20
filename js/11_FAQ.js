d0744bd8f64c83d46e3eee0443288a934beb5d0f
Nikitha Reddy <nikithareddy2309@gmail.com>
2021-10-20 05:12:07 +0000

    Header nav link on page done

---
+++ js/11_FAQ.js
@@ -0,0 +1,20 @@
+// This is included inside 11_FAQ.html
+var arrows = FAQ.querySelectorAll('.FAQ_arrow');
+var i;
+
+for (i = 0; i < arrows.length; i++) {
+  arrows[i].addEventListener("click", function() {
+	var answer = this.nextElementSibling;
+	if (answer.style.display === "block") {
+	  answer.style.display = "none";
+	} else {
+	  answer.style.display = "block";
+	}
+
+	if(this.style.transform=="rotate(-180deg)") {
+		this.style.transform="rotate(0deg)";
+	} else {
+		this.style.transform="rotate(-180deg)";
+	}
+  });
+}
--- js/index.js
+++ js/index.js
@@ -5,22 +5,3 @@ $(function () {
     $(this).load(file);
   });
 });
-const navLink = document.querySelectorAll('.Header nav ul li');
-const sections = document.querySelectorAll("div");
-window.addEventListener("scroll", () => {
-  let current = "";
-  sections.forEach((section) => {
-    const sectionTop = section.offsetTop;
-    const sectionHeight = section.clientHeight;
-    console.log(sectionHeight);
-    if (pageYOffset >= (sectionTop - sectionHeight/10)) {
-      current = section.getAttribute("id");
-    }
-  });
-  document.querySelectorAll('.Header nav ul li').forEach((li) => {
-    li.classList.remove("active");
-    if (li.classList.contains(current)) {
-      li.classList.add("active");
-    }
-  });
-});
