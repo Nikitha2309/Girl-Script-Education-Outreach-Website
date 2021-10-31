196f307e391cdc92602f9621ebe26830994a36d4
everly <everlyprecia@gmail.com>
2021-10-31 22:30:53 +0530

    add everly's updates

--- js/8_Register.js
+++ js/8_Register.js
@@ -1,56 +1,109 @@
 function next(){
-        document.getElementById('student-form2').style.left="15px";
-        document.getElementById('student-form2').style.right="15px";
-        document.getElementById('student-form1').style.left="1000px";
-        document.getElementById("student-form2").style.opacity = "1";
-        document.getElementById("student-form1").style.opacity = "0";
-    };
+    var input_fields=document.getElementsByClassName('valid-input1');
+    var flag=1;
+    for(var i=0;i<input_fields.length;i++){
+        if(input_fields[i].value.length==0){
+            flag=0;
+        }
+    }
+    if(flag==1){
+    document.getElementById('student-form2').style.left="15px";
+    document.getElementById('student-form2').style.right="15px";
+    document.getElementById('student-form1').style.left="1000px";
+    document.getElementById("student-form2").style.opacity = "1";
+    document.getElementById("student-form1").style.opacity = "0";
+    }
+    else{
+        alert('please fill all fields');
+    }
+    
+};
 
 function back(){
-    document.getElementById('student-form1').style.left="15px";
-    document.getElementById('student-form1').style.right="15px";
-    document.getElementById('student-form2').style.left="1000px";
-    document.getElementById('student-form2').style.opacity="0";
-    document.getElementById("student-form1").style.opacity = "1";
+document.getElementById('student-form1').style.left="15px";
+document.getElementById('student-form1').style.right="15px";
+document.getElementById('student-form2').style.left="1000px";
+document.getElementById('student-form2').style.opacity="0";
+document.getElementById("student-form1").style.opacity = "1";
 }
 
 function mentorform(){
-    document.getElementById('student-form1').style.left="2000px";
-    document.getElementById('student-form1').style.opacity="0";
-    document.getElementById('student-form2').style.left="2000px";
-    document.getElementById('student-form2').style.opacity="0";
-    document.getElementById('mentor-form1').style.left="15px";
-    document.getElementById('mentor-form1').style.right="15px";
-    document.getElementById('mentor-form1').style.opacity="1";
-    document.getElementById('mentor').style.backgroundColor="#EB5310";
-    document.getElementById('mentor').style.color="#ffffff";
-    document.getElementById('student').style.backgroundColor="#ffffff";
-    document.getElementById('student').style.color="#000000";
+document.getElementById('student-form1').style.left="2000px";
+document.getElementById('student-form1').style.opacity="0";
+document.getElementById('student-form2').style.left="2000px";
+document.getElementById('student-form2').style.opacity="0";
+document.getElementById('mentor-form1').style.left="15px";
+document.getElementById('mentor-form1').style.right="15px";
+document.getElementById('mentor-form1').style.opacity="1";
+document.getElementById('mentor').style.backgroundColor="#EB5310";
+document.getElementById('mentor').style.color="#ffffff";
+document.getElementById('student').style.backgroundColor="#ffffff";
+document.getElementById('student').style.color="#000000";
 }
 function studentform(){
-    document.getElementById('mentor-form1').style.left="2000px";
-    document.getElementById('mentor-form1').style.opacity="0";
-    document.getElementById('mentor-form2').style.left="2000px";
-    document.getElementById('mentor-form2').style.opacity="0";
-    document.getElementById('student-form1').style.left="15px";
-    document.getElementById('student-form1').style.right="15px";
-    document.getElementById('student-form1').style.opacity="1";
-    document.getElementById('student').style.backgroundColor="#EB5310";
-    document.getElementById('student').style.color="#ffffff";
-    document.getElementById('mentor').style.backgroundColor="#ffffff";
-    document.getElementById('mentor').style.color="#000000";
+document.getElementById('mentor-form1').style.left="2000px";
+document.getElementById('mentor-form1').style.opacity="0";
+document.getElementById('mentor-form2').style.left="2000px";
+document.getElementById('mentor-form2').style.opacity="0";
+document.getElementById('mentor-form3').style.left="2000px";
+document.getElementById('mentor-form3').style.opacity="0";
+document.getElementById('student-form1').style.left="15px";
+document.getElementById('student-form1').style.right="15px";
+document.getElementById('student-form1').style.opacity="1";
+document.getElementById('student').style.backgroundColor="#EB5310";
+document.getElementById('student').style.color="#ffffff";
+document.getElementById('mentor').style.backgroundColor="#ffffff";
+document.getElementById('mentor').style.color="#000000";
 }
 function mentornext() {
-    document.getElementById('mentor-form2').style.left="15px";
-    document.getElementById('mentor-form2').style.right="15px";
-    document.getElementById('mentor-form2').style.opacity="1";
-    document.getElementById('mentor-form1').style.left="1000px";
-    document.getElementById('mentor-form1').style.opacity="0";
+var input_fields=document.getElementsByClassName('valid-input2');
+var flag=1;
+for(var i=0;i<input_fields.length;i++){
+    if(input_fields[i].value.length==0){
+        flag=0;
+    }
+}
+if(flag==1){
+document.getElementById('mentor-form2').style.left="15px";
+document.getElementById('mentor-form2').style.right="15px";
+document.getElementById('mentor-form2').style.opacity="1";
+document.getElementById('mentor-form1').style.left="1000px";
+document.getElementById('mentor-form1').style.opacity="0";
+}
+else{
+    alert('please fill all fields');
+}
 }
 function mentorback(){
-    document.getElementById('mentor-form1').style.left="15px";
-    document.getElementById('mentor-form1').style.right="15px";
-    document.getElementById('mentor-form1').style.opacity="1";
-    document.getElementById('mentor-form2').style.left="1000px";
-    document.getElementById('mentor-form2').style.opacity="0";
+document.getElementById('mentor-form1').style.left="15px";
+document.getElementById('mentor-form1').style.right="15px";
+document.getElementById('mentor-form1').style.opacity="1";
+document.getElementById('mentor-form2').style.left="1000px";
+document.getElementById('mentor-form2').style.opacity="0";
+}
+function mentorsecondnext(){
+document.getElementById('mentor-form3').style.left="15px";
+document.getElementById('mentor-form3').style.right="15px";
+document.getElementById('mentor-form3').style.opacity="1";
+document.getElementById('mentor-form2').style.left="1000px";
+document.getElementById('mentor-form2').style.opacity="0";
+}
+function mentorsecondback(){
+var input_fields=document.getElementsByClassName('valid-input3');
+var flag=1;
+for(var i=0;i<input_fields.length;i++){
+    if(input_fields[i].value.length==0){
+        flag=0;
+    }
+}
+if(flag==1){
+document.getElementById('mentor-form2').style.left="15px";
+document.getElementById('mentor-form2').style.right="15px";
+document.getElementById('mentor-form2').style.opacity="1";
+document.getElementById('mentor-form3').style.left="1000px";
+document.getElementById('mentor-form3').style.opacity="0";
+}
+else{
+alert('please fill all fields');
+}
 }
--- js/index.js
+++ js/index.js
@@ -5,3 +5,9 @@ $(function () {
     $(this).load(file);
   });
 });
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+modal.style.display = "block";
+span.onclick = function() {
+  modal.style.display = "none";
+}
